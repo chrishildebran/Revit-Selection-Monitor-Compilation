1a0fd77731a6c0bdb2ee18d8ef1148a3b09cc4af
Chris Hildebran <32686889+ChrisHildebran@users.noreply.github.com>
2019-12-27 15:18:52 -0800

    Selection Changed Event Work

--- Application/Commands/SelectionChangedCommand.cs
+++ Application/Commands/SelectionChangedCommand.cs
@@ -2,7 +2,7 @@
 // Solution:............ Kelly Development
 // Project:............. BaseRevitModeless
 // File:................ SelectionChangedCommand.cs
-// Last Code Cleanup:... 12/27/2019 @ 12:12 PM Using ReSharper ✓
+// Last Code Cleanup:... 12/27/2019 @ 2:46 PM Using ReSharper ✓
 // /////////////////////////////////////////////////////////////
 namespace BaseRevitModeless.Commands
 {
@@ -14,10 +14,13 @@ namespace BaseRevitModeless.Commands
 	using System.Diagnostics;
 	using System.Linq;
 
+	using Autodesk.Revit.ApplicationServices;
 	using Autodesk.Revit.Attributes;
 	using Autodesk.Revit.DB;
 	using Autodesk.Revit.UI;
 	using Autodesk.Windows;
+
+	//using System.Windows.Controls.Ribbon;
 
 	[Transaction(TransactionMode.ReadOnly)]
 	internal class SelectionChangedCommand : IExternalCommand
@@ -25,9 +28,17 @@ namespace BaseRevitModeless.Commands
 
 		#region Fields (SC)
 
-		private static bool _subscribed;
+		private Application _rvtApp;
 
-		private static UIApplication _uiapp;
+		private ExternalCommandData _rvtCommandData;
+
+		private static Document _rvtDoc;
+
+		private UIApplication _rvtUiApp;
+
+		private UIDocument _rvtUiDoc;
+
+		private static bool _subscribed;
 
 		#endregion
 
@@ -35,7 +46,13 @@ namespace BaseRevitModeless.Commands
 
 		public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
 		{
-			_uiapp = commandData.Application;
+			{
+				_rvtCommandData = commandData;
+				_rvtApp         = commandData.Application.Application;
+				_rvtDoc         = commandData.Application.ActiveUIDocument.Document;
+				_rvtUiApp       = commandData.Application;
+				_rvtUiDoc       = commandData.Application.ActiveUIDocument;
+			}
 
 			foreach(var tab in ComponentManager.Ribbon.Tabs)
 			{
@@ -48,7 +65,7 @@ namespace BaseRevitModeless.Commands
 					}
 					else
 					{
-						//tab.PropertyChanged -= PanelEventOrigFromJT; // Added based on reading https://forums.xamarin.com/discussion/112676/eventhandler-not-being-removed-despite-being-unsubscribed
+						//tab.PropertyChanged -= PanelEvent; // Added based on reading https://forums.xamarin.com/discussion/112676/eventhandler-not-being-removed-despite-being-unsubscribed
 						tab.PropertyChanged += PanelEvent;
 						_subscribed         =  true;
 					}
@@ -63,30 +80,66 @@ namespace BaseRevitModeless.Commands
 		}
 
 
+		private static List<Element> GetElementInstancesFromProjectWhereTypeIdExistsOrdered(Document rvtDoc, List<ElementId> elementIds)
+		{
+			var collector = new FilteredElementCollector(rvtDoc, elementIds).WhereElementIsNotElementType();
+
+			return collector.Where(e => e.GetTypeId() != ElementId.InvalidElementId).OrderBy(e => e.Name).ToList();
+		}
+
+
 		private void PanelEvent(object sender, PropertyChangedEventArgs e)
 		{
+			Debug.WriteLine("--------------------------------------------------------------------------");
+
 			Debug.Print("Sender: " + sender);
 
 			Debug.Assert(sender is RibbonTab, "expected sender to be a ribbon tab");
 
-			if(e.PropertyName == "Title" && _subscribed)
+			if(e.PropertyName == "Title")
 			{
-				ICollection<ElementId> ids = _uiapp.ActiveUIDocument.Selection.GetElementIds();
+				List<ElementId> eOrdered = _rvtUiApp.ActiveUIDocument.Selection.GetElementIds().OrderBy(elementId => elementId.IntegerValue).ToList();
 
-				var n = ids.Count;
 
-				string s;
+				// Get instances
+				List<Element> iOrdered = null;
 
-				if(0 == n)
+				if(eOrdered.Count > 0)
 				{
-					s = "<nil>";
+					iOrdered = GetElementInstancesFromProjectWhereTypeIdExistsOrdered(_rvtDoc, eOrdered);
 				}
 				else
 				{
-					s = string.Join(", ", ids.Select(id => id.IntegerValue.ToString()));
+					return;
 				}
 
-				Debug.Print("CmdSelectionChanged: selection changed: " + s);
+				var n = eOrdered.Count;
+
+				var eidCount = 1;
+				var iidCount = 1;
+
+				string s1;
+				string s2;
+
+				if(0 == n)
+				{
+					s1 = "<nil>";
+					s2 = "<nil>";
+				}
+				else
+				{
+					s1 = string.Join(",  ", eOrdered.Select(id => id.IntegerValue + " [" + eidCount++ + "]"));
+					s2 = string.Join(",  ", iOrdered.Select(element => element.Id + " [" + iidCount++ + "]"));
+				}
+
+				Debug.WriteLine("");
+				Debug.Print("CmdSelectionChanged:  Element Id\'s: " + s1);
+				Debug.Print("CmdSelectionChanged: Instance Id\'s: " + s2);
+
+				if(!_subscribed)
+				{
+					Debug.Print("Why is the \'PanelEvent\' still being called when \'_subscribed\' = {0}", _subscribed);
+				}
 			}
 		}
 
